5100d1568ad9ec712dfdce99053c7e3ab378af74
Sujoytapu <ssujoy.t@gmail.com>
2022-04-22 21:27:45 +0600

    i have commited the blog project using php and mysql

---
+++ logic.php
@@ -0,0 +1,50 @@
+<?php
+    $conn = mysqli_connect("localhost", "root", "", "blog");
+
+    if(!$conn){
+        echo "<h3 class='container bg-dark text-center p-3 text-warning rounded-lg mt-5'>Not able establish Database Connection</h3>";
+    }
+
+    $sql = "SELECT * FROM `data`";
+    $query = mysqli_query($conn, $sql);
+
+
+    if(isset($_REQUEST["new_post"])){
+        $title = $_REQUEST["title"];
+        $content = $_REQUEST["content"];
+
+        $sql = "INSERT INTO `data`(`title`, `content`) VALUES('$title', '$content')";
+        mysqli_query($conn, $sql);
+
+        header("Location: index.php?info=added");
+        exit();
+    }
+
+    if(isset($_REQUEST['id'])){
+        $id = $_REQUEST['id'];
+
+        $sql = "SELECT * FROM `data` WHERE id=$id";
+        $query = mysqli_query($conn, $sql); 
+    }
+
+    if(isset($_REQUEST['update'])){
+        $id = $_REQUEST['id'];
+        $title = $_REQUEST['title'];
+        $content = $_REQUEST['content'];
+
+        $sql = "UPDATE `data` SET title = '$title', content = '$content' WHERE id = $id";
+        mysqli_query($conn, $sql);
+
+        header("location: index.php?info=updated");
+        exit();
+    }
+
+    if(isset($_REQUEST['delete'])){
+        $id = $_REQUEST['id'];
+
+        $sql = "DELETE FROM `data` WHERE id=$id";
+        $query = mysqli_query($conn, $sql); 
+
+        header("location: index.php?info=deleted");
+    }
+?>
